105877d2e65294b07bafa13388024317c57605be
Akram-HANY <ibnhany595@gmail.com>
2025-04-17 16:21:34 +0200

    finish

---
+++ main.js
@@ -0,0 +1,13 @@
+let img = document.getElementById('img');
+let container = document.getElementById('container');
+
+
+ function phones(phone){
+    img.src = phone;
+};
+
+function colors(color){
+    container.style.background  = color;
+
+
+};
